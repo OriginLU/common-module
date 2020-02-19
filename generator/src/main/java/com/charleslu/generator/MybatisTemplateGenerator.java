f77dd19049e83ec3984732b48b52549ed72429ec
fanchen <chl_it@126.com>
2020-02-19 16:59:35 +0800

    update:新增权限系统表设计

---
+++ generator/src/main/java/com/charleslu/generator/MybatisTemplateGenerator.java
@@ -0,0 +1,195 @@
+package com.charleslu.generator;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
+
+/**
+ * @author  ch.lu  
+ */
+public class MybatisTemplateGenerator {
+
+    public static void main(String[] args) {
+
+        /**
+         * 设置不生成@TableName 注解
+         */
+        AutoGenerator mpg = MybatisTemplateGenerator.getAutoGenerator();
+
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        gc.setAuthor("charles");
+        gc.setOutputDir(MybatisTemplateGenerator.getGeneratePath("rbac"));
+        // 是否覆盖同名文件，默认是false
+        gc.setFileOverride(true);
+        // 不需要ActiveRecord特性的请改为false
+        gc.setActiveRecord(true);
+        // XML 二级缓存
+        gc.setEnableCache(false);
+        // XML ResultMap
+        gc.setBaseResultMap(true);
+        // XML columList
+        gc.setBaseColumnList(true);
+
+        gc.setActiveRecord(false);
+
+        /* 自定义文件命名，注意 %s 会自动填充表实体属性！ */
+        // gc.setMapperName("%sDao");
+        // gc.setXmlName("%sDao");
+        // gc.setServiceName("MP%sService");
+        // gc.setServiceImplName("%sServiceDiy");
+        // gc.setControllerName("%sAction");
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setDbType(DbType.MYSQL);
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("123456");
+        dsc.setUrl("jdbc:mysql://internal.mysql.taoqicar.com:3306/test-rbac?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT%2B8");
+        mpg.setDataSource(dsc);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        // strategy.setCapitalMode(true);// 全局大写命名 ORACLE 注意
+        // 此处可以修改为您的表前缀
+        strategy.setTablePrefix("t");
+        // 表名生成策略
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityTableFieldAnnotationEnable(false);
+
+        strategy.setEntityLombokModel(true);
+//        strategy.setInclude(new String[] { "user" }); // 需要生成的表
+        // strategy.setExclude(new String[]{"test"}); // 排除生成的表
+        // 自定义实体父类
+        // strategy.setSuperEntityClass("com.baomidou.demo.TestEntity");
+        // 自定义实体，公共字段
+        // strategy.setSuperEntityColumns(new String[] { "test_id", "age" });
+        // 自定义 mapper 父类
+        // strategy.setSuperMapperClass("com.baomidou.demo.TestMapper");
+        // 自定义 service 父类
+        // strategy.setSuperServiceClass("com.baomidou.demo.TestService");
+        // 自定义 service 实现类父类
+        // strategy.setSuperServiceImplClass("com.baomidou.demo.TestServiceImpl");
+        // 自定义 controller 父类
+        // strategy.setSuperControllerClass("com.baomidou.demo.TestController");
+        // 【实体】是否生成字段常量（默认 false）
+        // public static final String ID = "test_id";
+        // strategy.setEntityColumnConstant(true);
+        // 【实体】是否为构建者模型（默认 false）
+        // public User setName(String name) {this.name = name; return this;}
+        // strategy.setEntityBuilderModel(true);
+        mpg.setStrategy(strategy);
+
+
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.charles.rbac");
+//        pc.setModuleName("test");
+        mpg.setPackageInfo(pc);
+
+        // 注入自定义配置，可以在 VM 中使用 cfg.abc 【可无】
+//        InjectionConfig cfg = new InjectionConfig() {
+//            @Override
+//            public void initMap() {
+//                Map<String, Object> map = new HashMap<String, Object>();
+//                map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
+//                this.setMap(map);
+//            }
+//        };
+//
+//        // 自定义 xxList.jsp 生成
+//        List<FileOutConfig> focList = new ArrayList<>();
+//        focList.add(new FileOutConfig("/template/list.jsp.vm") {
+//            @Override
+//            public String outputFile(TableInfo tableInfo) {
+//                // 自定义输入文件名称
+//                return "D://my_" + tableInfo.getEntityName() + ".jsp";
+//            }
+//        });
+//        cfg.setFileOutConfigList(focList);
+//        mpg.setCfg(cfg);
+//
+//        // 调整 xml 生成目录演示
+//        focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
+//            @Override
+//            public String outputFile(TableInfo tableInfo) {
+//                return "/develop/code/xml/" + tableInfo.getEntityName() + ".xml";
+//            }
+//        });
+//        cfg.setFileOutConfigList(focList);
+//        mpg.setCfg(cfg);
+//
+//        // 关闭默认 xml 生成，调整生成 至 根目录
+//        TemplateConfig tc = new TemplateConfig();
+//        tc.setXml(null);
+//        mpg.setTemplate(tc);
+
+        // 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/templates 下面内容修改，
+        // 放置自己项目的 src/main/resources/templates 目录下, 默认名称一下可以不配置，也可以自定义模板名称
+         TemplateConfig tc = new TemplateConfig();
+        // tc.setController("...");
+//         tc.setEntity("templates/entity.vm");
+         tc.setMapper("templates/mapper.xml.vm");
+        // tc.setXml("...");
+        // tc.setService("...");
+        // tc.setServiceImpl("...");
+        // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
+//         mpg.setTemplate(tc);
+
+        // 执行生成
+        mpg.execute();
+
+
+    }
+
+
+    public static AutoGenerator getAutoGenerator(){
+
+        return new AutoGenerator() {
+
+            @Override
+            public void execute() {
+
+                // 初始化配置
+                if (null == config) {
+                    config = new ConfigBuilder(getPackageInfo(), getDataSource(), getStrategy(), getTemplate(), getGlobalConfig());
+                    if (null != injectionConfig) {
+                        injectionConfig.setConfig(config);
+                    }
+                }
+                getConfig().getTableInfoList().forEach(tableInfo -> tableInfo.setConvert(false)
+                .getFields().forEach(e -> e.setKeyIdentityFlag(false)));
+                if (null == getTemplateEngine()) {
+                    // 为了兼容之前逻辑，采用 Velocity 引擎 【 默认 】
+                    setTemplateEngine(new VelocityTemplateEngine());
+                }
+                // 模板引擎初始化执行文件输出
+                getTemplateEngine().init(this.pretreatmentConfigBuilder(config)).mkdirs().batchOutput().open();
+            }
+        };
+    }
+
+
+
+
+
+    public static String getGeneratePath(String relativePath){
+
+        String path = MybatisTemplateGenerator.class.getResource("/").getPath();
+        return path.substring(0, path.lastIndexOf("target")) + relativePath;
+    }
+
+
+
+
+
+
+}
