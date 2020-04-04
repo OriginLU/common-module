3a037e07b37e389b81c97d2daaed1154b5a9d7a3
chlu <chl_it@126.com>
2020-04-04 23:36:30 +0800

    update:tenancy

--- tenancy/tenancy-client/src/main/java/com/zeroone/tenancy/hibernate/runner/HibernateTenancy.java
+++ tenancy/tenancy-client/src/main/java/com/zeroone/tenancy/hibernate/runner/HibernateTenancy.java
@@ -21,7 +21,6 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 
 public class HibernateTenancy implements SmartInitializingSingleton, InitializingBean {
