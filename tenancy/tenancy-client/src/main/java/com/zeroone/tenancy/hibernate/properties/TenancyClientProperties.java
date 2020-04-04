de6d18df707e5808e1b843594decb675d2511188
chlu <chl_it@126.com>
2020-04-04 23:41:19 +0800

    update:tenancy

--- tenancy/tenancy-client/src/main/java/com/zeroone/tenancy/hibernate/properties/TenancyClientProperties.java
+++ tenancy/tenancy-client/src/main/java/com/zeroone/tenancy/hibernate/properties/TenancyClientProperties.java
@@ -3,22 +3,12 @@ package com.zeroone.tenancy.hibernate.properties;
 import lombok.Data;
 import lombok.ToString;
 import lombok.experimental.Accessors;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.env.EnvironmentPostProcessor;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 import org.springframework.util.StringUtils;
-
-import java.util.Optional;
 
 @Data
 @Accessors(chain = true)
@@ -28,9 +18,6 @@ import java.util.Optional;
 public class TenancyClientProperties implements SmartInitializingSingleton {
 
     private static final String INSTANCE_NAME_KEY = "spring.application.name";
-
-
-    private  Environment environment;
 
     /**
      * 数据源服务名称
