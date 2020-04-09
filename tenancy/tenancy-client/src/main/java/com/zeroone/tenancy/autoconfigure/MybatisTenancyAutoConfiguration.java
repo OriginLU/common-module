73639323ba33ddc6c9bcc573a0e26d914b37214d
chlu <chl_it@126.com>
2020-04-09 11:11:19 +0800

    update:tenancy

--- tenancy/tenancy-client/src/main/java/com/zeroone/tenancy/autoconfigure/MybatisTenancyAutoConfiguration.java
+++ tenancy/tenancy-client/src/main/java/com/zeroone/tenancy/autoconfigure/MybatisTenancyAutoConfiguration.java
@@ -14,7 +14,9 @@ import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -26,6 +28,9 @@ import org.springframework.util.StringUtils;
 import javax.sql.DataSource;
 import java.util.List;
 
+@org.springframework.context.annotation.Configuration
+@ConditionalOnClass(MybatisProperties.class)
+@EnableConfigurationProperties(MybatisProperties.class)
 public class MybatisTenancyAutoConfiguration {
 
 
