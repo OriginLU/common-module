88e37c92758ac07345ada37122c6ce9978000554
chlu <chl_it@126.com>
2020-12-04 16:10:32 +0800

    update:文本编码识别

---
+++ lib/package-install.sh
@@ -0,0 +1,6 @@
+mvn install:install-file -DpomFile=./cpdetector-1.0.10.pom
+-DgroupId=info.monitorenter \
+-DartifactId=cpdetector \
+-Dversion=1.0.10 \
+-Dpackaging=jar \
+-Dfile=./cpdetector-1.0.10.jar
