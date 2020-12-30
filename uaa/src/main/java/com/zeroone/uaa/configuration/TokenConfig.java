7613c857116feda73b273eba19136a1c53bcb0cc
chlu <chl_it@126.com>
2020-12-30 21:39:08 +0800

    add:uaa

---
+++ uaa/src/main/java/com/zeroone/uaa/configuration/TokenConfig.java
@@ -0,0 +1,11 @@
+package com.zeroone.uaa.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zero-one.lu
+ * @since 2020-12-05
+ */
+@Configuration
+public class TokenConfig {
+}
