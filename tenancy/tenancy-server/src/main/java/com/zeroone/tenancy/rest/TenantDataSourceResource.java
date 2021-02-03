9cc6f0bd8003d626974af3b49241e44fbd48dc7a
chlu <chl_it@126.com>
2021-02-03 13:51:21 +0800

    update:tenancy

--- tenancy/tenancy-server/src/main/java/com/zeroone/tenancy/rest/TenantDataSourceResource.java
+++ tenancy/tenancy-server/src/main/java/com/zeroone/tenancy/rest/TenantDataSourceResource.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tenant/data-source")
+@RequestMapping("/api/tenant/data-source")
 public class TenantDataSourceResource {
 
 
