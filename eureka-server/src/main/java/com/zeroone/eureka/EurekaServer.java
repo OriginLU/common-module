341f02c8ad299e2fcaf1da9fe39d36d056e34d6e
chlu <chl_it@126.com>
2021-03-23 15:58:43 +0800

    update:spring security

--- eureka-server/src/main/java/com/zeroone/eureka/EurekaServer.java
+++ eureka-server/src/main/java/com/zeroone/eureka/EurekaServer.java
@@ -3,6 +3,10 @@ package com.zeroone.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
 @SpringBootApplication
@@ -11,5 +15,17 @@ public class EurekaServer {
     public static void main(String[] args) {
 
         SpringApplication.run(EurekaServer.class, args);
+    }
+
+
+    @EnableWebSecurity
+    static class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
     }
 }
--- lib/package-install.sh
+++
@@ -1,6 +0,0 @@
-mvn install:install-file -DpomFile=./cpdetector-1.0.10.pom
--DgroupId=info.monitorenter \
--DartifactId=cpdetector \
--Dversion=1.0.10 \
--Dpackaging=jar \
--Dfile=./cpdetector-1.0.10.jar
